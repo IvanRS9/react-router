8d1ce1b4f82e29fe8934daeae47cfc1fd7af6e21
Chistoper Ramirez <19002167@alumnos.utleon.edu.mx>
2024-11-15 17:19:22 -0600

    feat: protected routes w/ outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
@@ -8,21 +8,68 @@ import Users from './pages/Users'
 import UserPage from './pages/UserPage'
 import Dashboard from './pages/Dashboard'
 import Welcome from './components/Welcome'
+import ProtectRoute from './components/ProtectRoute'
 
 const App = () => {
+  const [user, setUser] = useState(null)
+
+  const login = () => {
+    setUser({ id: 1, username: 'admin', permissions: ['admin'], roles: ['adm'] })
+  }
+
+  const logout = () => {
+    setUser(null)
+  }
+
   return (
     <div>
       <BrowserRouter>
         <Navbar />
+        {
+          user ? (
+            <button onClick={logout}>Logout</button>
+          ) : (
+            <button onClick={login}>Login</button>
+          )
+        }
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/:id" element={<UserPage />} />
-          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          <Route path='/dashboard/*' element={<Dashboard />}>
-            <Route path='welcome' element={<Welcome />} />
+
+          {/* Ruta protegida con outlet */}
+          <Route element={<ProtectRoute isAllowed={!!user} />}>
+            <Route path='/users' element={<Users />} />
+            <Route path='/dashboard/*' element={<Dashboard />} >
+              <Route path='welcome' element={<Welcome />} />
+            </Route>
           </Route>
+
+          {/* Ruta protegida por roles */}
+          <Route path="/users/:id" element={
+            <ProtectRoute isAllowed={!!user && user.roles.includes('adm')}>
+              <UserPage />
+            </ProtectRoute>
+          } />
+
+          {/* Ruta protegida individual y por permisos */}
+          <Route path="/about" element={
+            <ProtectRoute isAllowed={!!user && user.permissions.includes('admin')} redirectTo='/'>
+              <AboutPage />
+            </ProtectRoute>
+          } />
+
+
+          {/* <Route path="/users" element={
+            // <ProtectRoute user={user} redirectTo='/'>
+            <Users />
+            // </ProtectRoute>
+          } />
+          <Route path='/dashboard/*' element={<Dashboard />} >
+            <Route path='welcome' element={
+              // <ProtectRoute user={user} redirectTo='/'>
+              <Welcome />
+              // </ProtectRoute>
+            } />
+          </Route> */}
 
           <Route path='/usuarios' element={<Navigate replace to="/users" />} />
 
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
 const Users = () => {
+    const users = [
+        { id: 1, name: 'John Doe' },
+        { id: 2, name: 'Jane Doe' },
+        { id: 3, name: 'Bruce Wayne' },
+        { id: 4, name: 'Clark Kent' },
+    ]
+
     return (
         <div>
             <h1>Users</h1>
+            <br />
+            <ul>
+                {
+                    users.map(user => (
+                        <li key={user.id}>{user.name}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
